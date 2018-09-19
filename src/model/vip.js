052cfc9d3ad0a308b5a34600547634b949e235f3
leafront <yeliang@laiyifen.com>
2018-09-19 13:54:10 +0800

    update vip

--- src/model/vip.js
+++ src/model/vip.js
@@ -6,3 +6,7 @@ export const getDolphinList = (data) => request('/api/dolphin/list', data)
 export const getRankList = (data) => request('/api/ranking/rangKingList')
 
 export const getRankPrice = (data) => request('/api/realTime/getPriceStockList')
+
+export const getUserInfo = (data) => request('/api/my/user/info', data)
+
+export const getWalletInfo = (data) => request('/api/my/wallet/summary', data)
