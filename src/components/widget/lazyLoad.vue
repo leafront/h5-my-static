f3befcdc87e2c1fa5356dc6de62a7508b0073cb6
YELIANG <yeliang@laiyifen.com>
2018-10-19 10:49:06 +0800

    update lazyLoad.vue

--- src/components/widget/lazyLoad.vue
+++ src/components/widget/lazyLoad.vue
@@ -9,15 +9,10 @@
   import utils from '@/widget/utils'
 
   export default {
-
     props: ['options','list'],
-
     data () {
-
       return {
-
         appView: null,
-
         default: {
           scrollEle: 'appView',
           ele:'pic-lazyLoad',
@@ -31,162 +26,97 @@
           }
         }
       }
-
     },
-
     created () {
-
       Object.assign(this.default, this.options)
-
     },
-
     mounted () {
-
       this.appView = document.getElementById(this.default.scrollEle)
 
     },
-
     watch: {
-
       list () {
-
         setTimeout(() => {
-
           this.startLoad()
-
         },0)
-
       }
-
     },
-
     beforeDestroy () {
-
       this.appView.removeEventListener('scroll',this.scrollImg,false)
-
     },
-
     methods: {
-
       /**
-       *
        * @param {Object} el
-       *
        * @param {Object} options
-       *
        * @returns {Boolean}
        *
        */
-
       getClientRect (el,options) {
 
         const bcr = el.getBoundingClientRect() //取得元素在可视区的位置
-
         const mw = el.offsetWidth //元素自身宽度
-
         const mh = el.offsetHeight //元素自身的高度
-
         const w = window.innerWidth //视窗的宽度
-
         const h = window.innerHeight //视窗的高度
-
         const boolX = (!((bcr.right - options.left) <= 0 && ((bcr.left + mw) - options.left) <= 0) && !((bcr.left + options.right) >= w && (bcr.right + options.right) >= (mw + w))) //上下符合条件
-
         const boolY = (!((bcr.bottom - options.top) <= 0 && ((bcr.top + mh) - options.top) <= 0) && !((bcr.top + options.bottom) >= h && (bcr.bottom + options.bottom) >= (mh + h))) //上下符合条件
-
         return el.width != 0 && el.height != 0 && boolX && boolY
-
       },
-
       /**
        * 滚动加载图片显示替换默认图片
        *
-       * @param null
-       *
-       * @return null
-       *
        */
-
       scrollLoad () {
 
         const list = Array.prototype.slice.apply(this.appView.querySelectorAll('.' + this.default.ele + '[data-src]'))
 
         if (!list.length && this.default.complete) {
-
           this.appView.removeEventListener('scroll',this.scrollImg,utils.isPassive() ? {passive: true} : false)
-
         } else {
-
           list.forEach((el) => {
-
             if (!el.dataset.LazyLoadImgState && this.getClientRect(el, this.default.position)) {
-
               this.loadImg(el)
-
             }
           })
         }
       },
-
       scrollImg () {
 
         utils.throttle(() => {
-
           this.scrollLoad()
-
         },this.default.time)()
-
       },
-
       startLoad (){
-
         this.scrollLoad()
-
         this.appView.addEventListener('scroll',this.scrollImg,utils.isPassive() ? {passive: true} : false)
-
       },
       /**
        * @param {Object} el
        *
        *
        */
-
       loadImg(el) { //加载图片
 
         el.dataset.LazyLoadImgState = 'start'
-
         const imgUrl = el.dataset.src
 
         if (imgUrl) {
-
           const img = new Image()
-
           img.src = imgUrl
-
           img.addEventListener('load', () => {
-
             setTimeout(() => {
-
               el.style.backgroundImage = 'url('+imgUrl+')'
-
               el.style.backgroundSize = '100% auto'
-
               delete el.dataset.src
-
               el.dataset.LazyLoadImgState = 'success'
-
               el.classList.add('successImg')
-
             },150)
 
           }, false)
 
           img.addEventListener('error', () => {
-
             delete el.dataset.src
-
             el.dataset.LazyLoadImgState = 'error'
-
           }, false)
         } else {
           delete el.dataset.src
