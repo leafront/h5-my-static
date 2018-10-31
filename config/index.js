0039aa40c76fc8426245b1a35adb44bbadc2a77e
yeliang <yeliang@laiyifen.com>
2018-10-31 16:58:39 +0800

    update config

--- config/index.js
+++ config/index.js
@@ -161,7 +161,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../target'),
     assetsSubDirectory: 'my-static',
-    assetsPublicPath: 'https://mstatic.laiyifen.com/',
+    assetsPublicPath: 'https://m.laiyifen.com/',
 
     /**
      * Source Maps
