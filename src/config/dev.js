9af9e419910a94ce3c3547eac5f507daf4e55879
YELIANG <yeliang@laiyifen.com>
2018-11-07 20:40:33 +0800

    uapte config

--- src/config/dev.js
+++ src/config/dev.js
@@ -5,7 +5,8 @@ const config = JSON.stringify({
   "staticPath": "http://static.lyf.edu.laiyifen.com",
   "appKey": "9ca572cc88e6",
   "platform": 2,
-  "getTime": new Date().getTime()
+  "getTime": new Date().getTime(),
+   "QYKeFuKey": "5690717053c82f04333c095b681c5121"
 })
 
 module.exports = config
--- src/config/test.js
+++ src/config/test.js
@@ -6,7 +6,8 @@ const config = JSON.stringify({
   "appKey": "9ca572cc88e6",
   "platform": 2,
   "getTime": new Date().getTime(),
-  "originalId": "gh_4a4a36db4c12"
+  "originalId": "gh_4a4a36db4c12",
+  "QYKeFuKey": "5690717053c82f04333c095b681c5121"
 })
 
 module.exports = config
