0efbab3863ded1f167541758d2da6febd947a9b9
YELIANG <yeliang@laiyifen.com>
2018-12-13 17:58:04 +0800

    update config

--- build/webpack.release.conf.js
+++ build/webpack.release.conf.js
@@ -74,7 +74,8 @@ const webpackConfig = merge(baseWebpackConfig, {
       minify: {
         removeComments: true,
         collapseWhitespace: true,
-        removeAttributeQuotes: true
+        removeAttributeQuotes: true,
+        minifyJS: true
         // more options:
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
