7c831c684a25c394e330b7c0e627241ab64434b2
YELIANG <yeliang@laiyifen.com>
2018-12-27 16:32:01 +0800

    udpate config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,29 @@
 module.exports = {
-  rules: {
-   'no-console': 'off'
-  },
+  root: true,
   parserOptions: {
-		sourceType: 'module',
-    allowImportExportEverywhere: true
+    sourceType: 'module',
+    ecmaVersion: 8,
   },
-  plugins: ['vue']
+  extends: [
+    'standard',
+    'plugin:vue/recommended', // or 'plugin:vue/base'
+  ],
+  'env': {
+    'browser': true,
+  },
+  // add your custom rules here
+  'rules': {
+    'eqeqeq': ['error', 'always'],
+    'indent': ['error', 2],
+    'semi': ['error', 'always'],
+    'space-before-function-paren': ['error', 'never'],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // trailing comma
+    'comma-dangle': ['error', 'always-multiline'],
+  }
 }
--- vue.config.js
+++ vue.config.js
@@ -50,9 +50,9 @@ module.exports = {
       }))
     }
   },
-  baseUrl: process.env.VUE_APP_STATIC_PATH,// 部署应用时的根路径(默认'/'),也可用相对路径(存在使用限制)
+  baseUrl: process.env.NODE_ENV == 'production' ? process.env.VUE_APP_STATIC_PATH : '/',// 部署应用时的根路径(默认'/'),也可用相对路径(存在使用限制)
   outputDir: 'target',// 运行时生成的生产环境构建文件的目录(默认''dist''，构建之前会被清除)
-  assetsDir: './my-static',//放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
+  assetsDir: 'my-static',//放置生成的静态资源(s、css、img、fonts)的(相对于 outputDir 的)目录(默认'')
   indexPath: 'index.html',//指定生成的 index.html 的输出路径(相对于 outputDir)也可以是一个绝对路径。
   productionSourceMap: false,
   lintOnSave: true,
