8769ab68206d84c9097627f444950d68a2f1d357
YELIANG <yeliang@laiyifen.com>
2019-02-13 15:50:43 +0800

    update lazyLoad.vue

--- src/components/widget/lazyLoad.vue
+++ src/components/widget/lazyLoad.vue
@@ -18,7 +18,6 @@
         default: {
           scrollEle: 'appView',
           ele:'pic-lazyLoad',
-          time: 100, // 设置一个检测时间间隔
           complete: true, //页面内所有数据图片加载完成后，是否自己销毁程序，true默认销毁，false不销毁
           position: { // 只要其中一个位置符合条件，都会触发加载机制
             top: 0, // 元素距离顶部
